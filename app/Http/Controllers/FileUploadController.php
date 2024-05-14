d84bab727c1afa30aca94560ef3102c2e6d4de1b
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-05-14 10:53:00 +0700

    Jobsheet 12_Praktikum E: Membuat Symlink

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -21,8 +21,11 @@ class FileUploadController extends Controller
         ]);
         $extFile = $request->berkas->getClientOriginalName();
         $namaFile = "web-" . time() . "." . $extFile;
-        $path = $request->berkas->storeAs('uploads', $namaFile);
-        echo "Proses upload berhasil, file berada di: " . $path;
-        // echo $request->berkas->getClientOriginalName() . " lolos validasi";
+        $path = $request->berkas->storeAs('public', $namaFile);
+
+        $pathBaru = asset('storage/' . $namaFile);
+        echo "proses upload berhasil, data disimpan pada:$path";
+        echo "<br>";
+        echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
     }
 }
