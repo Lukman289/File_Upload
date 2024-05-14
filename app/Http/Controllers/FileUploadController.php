fd7f865076fc67d346579312ce6d3d16087af3e8
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-05-14 10:39:48 +0700

    Jobsheet 12_Praktikum D: Memindah File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -19,6 +19,10 @@ class FileUploadController extends Controller
         $request->validate([
             'berkas' => 'required|file|image|max:5000'
         ]);
-        echo $request->berkas->getClientOriginalName() . " lolos validasi";
+        $extFile = $request->berkas->getClientOriginalName();
+        $namaFile = "web-" . time() . "." . $extFile;
+        $path = $request->berkas->storeAs('uploads', $namaFile);
+        echo "Proses upload berhasil, file berada di: " . $path;
+        // echo $request->berkas->getClientOriginalName() . " lolos validasi";
     }
 }
